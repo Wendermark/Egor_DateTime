40fae356e416ccfbecc7f87f388d1f5dcb798a08
Wendermark <caseharo.e@gmail.com>
2021-10-14 12:25:06 +0300

    Добавьте файлы проекта.

---
+++ Egor_DateTime/BaseElements/Date.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Egor_DateTime.BaseElements
+{
+    struct Date
+    {
+
+        public Date(int day, int month, int year)
+        {
+            Day = day;
+            Month = month;
+            Year = year;
+        }
+
+        public int Day { get; private set; }
+
+        public int Month { get; private set; }
+
+        public int Year { get; private set; }
+
+        public static int operator - (Date firstDate , Date secondDate) => (firstDate.Day + firstDate.Month*31 + firstDate.Year*365) - (secondDate.Day + secondDate.Month*31 + secondDate.Year*365);
+
+        public void ShowDate() => Console.WriteLine($"День = {Day.ToString()}, Месяц = {Month.ToString()}, Год = {Year.ToString()}");
+        
+        public static Date operator + (Date date, int days)
+        {
+            int year = date.Year;
+
+            int month = date.Month;
+
+            int day = date.Day + days;
+
+            if (day > 31)
+            {
+                day -= 31;
+
+                month += 1;
+
+                    if (month > 12)
+                    {
+                        month -= 12;
+
+                        year += 1;
+                    }
+            }
+
+            return new Date(day, month, year);
+        }
+
+
+    }
+}
---
+++ Egor_DateTime/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Egor_DateTime.BaseElements;
+
+namespace Egor_DateTime
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var date = new Date(10, 5, 2004);
+
+            var secondDate = new Date(20, 5, 2004);
+
+            date += 5;
+
+            date.ShowDate();
+
+            Console.WriteLine((secondDate - date).ToString());
+
+
+            Console.ReadKey();
+        }
+    }
+}
